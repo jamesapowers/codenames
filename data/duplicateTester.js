d60ce52b70b5231bf22b434f54db3bb42a0d054a
Kari Loftesness <kariloftesness@Karis-iMac.local>
2018-04-17 13:58:05 -0700

    added word list and duplicate tester

---
+++ data/duplicateTester.js
@@ -0,0 +1,18 @@
+const test = require('./default.js');
+
+const RunTest = (words) => {
+  let usedWords = {};
+  let repeatedWords = [];
+  
+  for (let word of words) {
+    if (usedWords[word]) {
+      repeatedWords.push(word);
+    } else {
+      usedWords[word] = 1;
+    }
+  }
+
+  return repeatedWords;
+}
+
+console.log(RunTest(test));
