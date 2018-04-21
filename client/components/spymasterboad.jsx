0c997bdee2c6f927e839b5e684bcce7bc18dd8ab
James Powers <powersajames@gmail.com>
2018-04-21 10:41:18 -0700

    created drawCards

--- client/components/spymasterboad.jsx
+++ client/components/spymasterboad.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import Row from './row';
+import wordBank from '../../data/default';
 
 class SpyMasterboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      board: this.createBoard(),
+      board: this.createBoard(5),
     };
 
     this.onTileClick = this.onTileClick.bind(this);
   }
 
-  createBoard() {
+  createBoard(size) {
+    const cards = drawCards(size * size);
 
+  }
+
+  drawCards(amount) {
+    let cards = new Array(amount);
+    let selected = {n: 1};
+    return cards.map(card => {
+      let word = 'n';
+      while (selected[word]) {
+        word = wordBank[Math.floor(Math.random() * wordBank.length)];
+      }
+      return word;
+    });
   }
 
   onTileClick(tile) {
