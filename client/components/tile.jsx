e07063b9f07c66bd269632d52d8232809723e330
James Powers <powersajames@gmail.com>
2018-05-03 09:53:00 -0700

    completed Tile

--- client/components/tile.jsx
+++ client/components/tile.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const Tile = ({ tile, onTileClick }) => {
+const Tile = ({ tile, onTileClick, tileLoc }) => {
+  let tileDisplay = tile.word;
+  if (tile.status !== 'blank') {
+    tileDisplay = '';
+  }
   return (
     <div>
-      <div>
-        {tile.word}
-        {onTileClick}
+      <div
+        className={tile.status}
+        onClick={ () => (onTileClick(tileLoc)) }>
+        {tileDisplay}
       </div>
     </div>
   );
