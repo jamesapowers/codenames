a6ff4aa5085c54daf2d882ca8ede5ce41b0ea29b
James Powers <powersajames@gmail.com>
2018-06-28 14:48:14 -0700

    position added

--- client/components/gamemodes.jsx
+++ client/components/gamemodes.jsx
@@ -6,12 +6,12 @@ const Gamemodes = ({ onGamemodeClick }) => {
   return (
     <div className="gamemodes">
       {
-        row.map((gamemode) => {
+        row.map((gamemode, idx) => {
           return (
               <Gamemode
                 onGamemodeClick={onGamemodeClick}
                 gamemode={gamemode}
-                >
+                position={idx}>
               </Gamemode>
           );
         });
