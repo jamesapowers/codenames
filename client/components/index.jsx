7ce41806d092939e06bea7e349b2ec73cd7194b7
James Powers <powersajames@gmail.com>
2018-07-02 17:14:31 -0700

    gamemodes array created

--- client/components/index.jsx
+++ client/components/index.jsx
@@ -11,6 +11,7 @@ class App extends React.Component {
     this.state = {
       data: data,
       gameboard: true,
+      gamemodes: 'Classic',
     };
     this.onTileClick = this.onTileClick.bind(this);
   }
