95a775ff4fdcf691ee957dbf1b837332f1885b0d
James Powers <powersajames@gmail.com>
2018-08-13 14:27:31 -0700

    added top movies for 2010s

--- data/movies.js
+++ data/movies.js
@@ -1,3 +1,22 @@
 module.exports = [
-  
+  //2010s top 50 Movies
+  'Star Wars: The Force Awakens', 'Avengers: Infinity War', 'Jurassic World',
+  'The Avengers', 'Furious 7', 'Avengers: Age of Ultron', 'Black Panther',
+  'Harry Potter and the Deathly Hallows', 'Star Wars: The Last Jedi',
+  'Frozen', 'Beauty and the Beast', 'The Fate of the Furious', 'Iron Man 3',
+  'Minions', 'Captain America: Civil War', 'Transformers: Dark of the Moon',
+  'Skyfall', 'Transformers: Age of Extinction', 'The Dark Knight Rises',
+  'Toy Story 3', 'Rogue One: A Star Wars Story',
+  'Pirates of the Caribbean: On Stranger Tides', 'Finding Dory',
+  'Alice in Wonderland', 'Zootopia', 'The Hobbit: An Unexpected Journey',
+  'Despicable Me 3', 'Despicable Me 2', 'The Jungle Book',
+  'Harry Potter and the Deathly Hallows – Part 1', 'The Hobbit: The Desolation of Smaug',
+  'The Hobbit: The Battle of the Five Armies', 'Spectre', 'Ice Age: Continental Drift',
+  'The Secret Life of Pets', 'Batman v Superman: Dawn of Justice',
+  'The Hunger Games: Catching Fire', 'Guardians of the Galaxy Vol. 2', 'Inside Out',
+  'Wolf Warriors 2', 'The Twilight Saga: Breaking Dawn – Part 2', 'Inception',
+  'Spider-Man: Homecoming', 'Wonder Woman', 'Fantastic Beasts and Where to Find Them',
+  'Pirates of the Caribbean: Dead Men Tell No Tales', 'Fast & Furious 6', 'Deadpool',
+  'Guardians of the Galaxy', 'Maleficent',
+  //2000s top 50 Movies
 ];
